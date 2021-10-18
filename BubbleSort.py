18ebeed178e2a23186e71dcbaa095851941de791
MadhushaPrasad <madushaprasad21@gmail.com>
2021-10-19 02:08:13 +0530

    bubble sort in python #47

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,26 +1,15 @@
-# Python program for implementation of Bubble Sort
+def bubble_sort(lst):
+    flag = True
+
+    while flag :
+        flag = False
+        for i in range(len(lst)-1):
+            if lst[i] > lst[i+1]:
+                lst[i],lst[i+1] = lst[i+1],lst[i]
+                flag = True
 
 
-def bubbleSort(arr):
-    n = len(arr)
+lst = [1,5,2,7]
 
-    # Traverse through all array elements
-    for i in range(n):
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
-
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-
-if __name__ == '__main__':
-
-    # test array
-    arr = [13, 20, 5, 8, 1, 3, 25]
-    bubbleSort(arr)
-
-    # prints sorted array
-    print(arr)
+bubble_sort(lst)
+print(lst)
